6054943ea0191192f23b9162acd65dc9d7e1c0cc
takano697 <106021983+takano697@users.noreply.github.com>
2023-10-28 18:40:14 +0900

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,4 +1,4 @@
-import { THREE } from './three.module.js';
+import * as THREE from './three.module.js';
 import { GLTFLoader } from "./GLTFLoader.js";
 import { FlyControls } from "./FlyControls.js";
 
